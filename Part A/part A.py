6da58156bf867e8be9c825b478a63b1107d2deb4
Assia Khateeb <39053503+assiakhateeb@users.noreply.github.com>
2021-05-20 03:51:58 +0300

    Update part A.py

--- Part A/part A.py
+++ Part A/part A.py
@@ -26,5 +26,21 @@ print(X)
 Y = pd.DataFrame(iris.target, columns=["Species"])
 print(Y)
 # Now let's fit a DecisionTreeClassifier instance
-tree = DecisionTreeClassifier(max_depth=2)
-tree.fit(X,Y)
+tree = DecisionTreeClassifier(max_depth=5)
+tree.fit(X, Y)
+# Visualizing the build Decision tree
+from sklearn.tree import export_graphviz
+
+""" gini is a measure that is used to find a node to split on it
+    samples refer to the number of samples that are entering this node
+    value is a split of the data instances according to their class 
+     example: value = [instances_num_of_setosa, instances_num_of_versicolor, instances_num_of_virginica
+    class is what the decision tree would predict
+"""
+# Creates dot file named tree.dot
+export_graphviz(tree, out_file="myTree.dot", feature_names=list(X.columns),
+                class_names=iris.target_names,
+                filled=True, rounded=True)
+
+# convert .dot file to png
+""" https://onlineconvertfree.com/complete/dot-png/ """
