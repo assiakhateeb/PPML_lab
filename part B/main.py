2aaeccb11782670dc9a96a5176e0c1506aeaafab
Assia Khateeb <39053503+assiakhateeb@users.noreply.github.com>
2021-06-28 13:36:04 +0300

    Update main.py

--- part B/main.py
+++ part B/main.py
@@ -3,16 +3,23 @@ import sys
 
 
 def main(argv):
-    data_type = str(input("Please select a data type(iris/wine): "))
-    print('data_type =', data_type)
-    myTree, phi, X_test, Y_test, sklearn_tree = pre_processing(data_type)
-    print('\n', '--------print tree--------')
-    printTree(myTree)
-    print('\n', '--------now prediction--------')
+    select_data_type = input("Please select a data type(iris/wine/cancer) or type exit to stop: ")
+    while select_data_type.lower() != 'exit':
+        try:
+            data_type = str(select_data_type)
+            print('data_type =', data_type)
+            myTree, phi, X_test, Y_test, sklearn_tree = pre_processing(data_type)
+            print('\n', '--------print tree--------')
+            printTree(myTree)
+            print('\n', '--------now prediction--------')
 
-    calc_algorithm1_accuracy(myTree, phi, X_test, Y_test)
-    sklearn_score(sklearn_tree, X_test, Y_test)
-    print_tree(sklearn_tree, data_type)
+            calc_algorithm1_accuracy(myTree, phi, X_test, Y_test)
+            sklearn_score(sklearn_tree, X_test, Y_test)
+            print_tree(sklearn_tree, data_type)
+
+        except:
+            print("UnExpected Error: ", sys.exc_info()[0])
+        select_data_type = input("Please select a data type(iris/wine/cancer) or type exit to stop: ")
 
 
 if __name__ == '__main__':
